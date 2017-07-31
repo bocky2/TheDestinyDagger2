88e84847cdc275e7870bd256605a6f49645af386
bocky2 <pokedoug@hotmail.com.au>
2017-07-31 17:48:40 +1000

    Finalisation of prototype
    All done! On to master...

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public int score;
+    public int multiplier;
+    public Text scoreText;
+    private bool scoreRun = true;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Space) == true)
+        {
+            scoreRun = false;
+        }
+
+        if (scoreRun == true)
+        {
+            score += 1;
+
+            scoreText.text = score.ToString();
+        }
+	}
+}
--- TheDestinyDagger2/Assets/Scripts/UI/UIManager.cs
+++ TheDestinyDagger2/Assets/Scripts/UI/UIManager.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour {
 
+	//Set variable for the score value
     public int score;
+	//Set variable for the score multiplier
     public int multiplier;
+	//Set variable for the text of the score
     public Text scoreText;
+	//Set variable for whether the score is running or not.
     private bool scoreRun = true;
 
 	// Use this for initialization
@@ -19,15 +23,19 @@ public class UIManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+		//Set statement for if the space key is pressed
         if (Input.GetKey(KeyCode.Space) == true)
         {
             scoreRun = false;
         }
-
+		
+		//Set statement for if scoreRun is true
         if (scoreRun == true)
         {
-            score += 1;
+			//Add to the score at a constant rate
+            score += multiplier;
 
+			//Set the text variable to display the score
             scoreText.text = score.ToString();
         }
 	}
